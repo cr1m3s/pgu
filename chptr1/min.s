86504ef43454fa104f71baefd3d9810c7449afda
O P <luv_cr1m3s@protonmail.com>
2024-08-20 22:29:51 +0200

    chptr 3 done

--- chptr1/min.s
+++ chptr1/min.s
@@ -1,38 +1,23 @@
 .section .data
 data_items:
-    .long 3, 67, 123, 1, 23, 43, 7, 44, 26, 222, 255
-
-count:
-		.long 11
+    .long 3, 67, 123, 1, 23, 43, 3, 44, 26, 222, 0
 
 .section .text
 
 .globl _start
 _start:
     movl $0, %edi
-#		movl $11, %edx
     movl data_items(, %edi, 4), %eax
     movl %eax, %ebx
 
-		movl $data_items, %ecx
-		movl count, %edx
-		imul $4, %edx
-		addl %ecx, %edx
-
 start_loop:
-		cmpl %ecx, %edx
-		je loop_exit
-#		cmpl %edx, %edi
-#		je loop_exit
-
     incl %edi
     movl data_items(,%edi, 4), %eax
-		add $4, %ecx
 
-#		cmpl $255, %eax
-#    je loop_exit
+		cmpl $0, %eax
+		je loop_exit
 
-		cmpl %ebx, %eax
+		cmpl %eax, %ebx
     jle start_loop
 
     movl %eax, %ebx
