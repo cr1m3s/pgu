781799a86337f3dade1926b81d79ceb161ed2993
O P <luv_cr1m3s@protonmail.com>
2024-08-21 21:56:02 +0200

    started functions

---
+++ chptr4/power.s
@@ -0,0 +1,45 @@
+.section .data
+.section .text
+
+.globl _start
+_start:
+    pushl $3
+    pushl $2
+    call power
+		add $8, %esp
+		push %eax
+    movl %eax, %ebx
+
+    movl $1, %eax
+    int $0x80
+
+
+    .type power, @function
+    power:
+            pushl %ebp
+            movl %esp, %ebp
+
+            movl 8(%ebp), %ebx          # ebx = number
+            movl 12(%ebp), %ecx         # ecx = power
+
+            subl $4, %esp               # get room for local storage
+            movl %ebx, -4(%ebp)         # save current result in local storage
+
+            power_loop_start:
+                cmpl $1, %ecx           # if power = 1
+                je end_power          # jump end of function
+
+                movl -4(%ebp), %eax     # save current result in temp storage
+                imull %ebx, %eax        # current result = current result * number
+                movl %eax, -4(%ebp)     # save current result in local storage
+
+                decl %ecx               # decrement power
+                jmp power_loop_start
+
+            movl -4(%ebp), %eax         # set result in return value
+
+        end_power:
+						movl -4(%ebp), %eax
+            movl %ebp, %esp
+            popl %ebp
+            ret
